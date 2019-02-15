9a425816ba00686181b6749f5bfdccaea80efb5d
root <1556258098@qq.com>
2019-02-15 16:59:03 +0800

    2019/2/15

--- 简单的内存池/Memory-pool.cpp
+++ 简单的内存池/Memory-pool.cpp
@@ -31,8 +31,83 @@ class ObjectPool
    static size_t SingleObjNum; // 单个对象的大小
    BlockNode *_start; // 内存链表的初始位置
    BlockNode * _end; // 内存链表的尾部位置
-   size_t _cout ;// 内存链表结点的个数
+   size_t _cout ;// 当前节点在用的个数
+   T* _lastDelete; // 指向最后一个释放的对象
+   size_t _maxcap;  //单个结点的最大容量
+
+   public:
+           ObjectPool(size_t num = 62,size_t maxcap = 1000)
+              :_cout(0)
+               ,_lastDelete(NULL)
+               ,_maxcap(1000)
+
+           {
+                   _start = _end = new BlockNode(num);
+           }
+           ~ObjectPool()
+           {
+             Destory();
+           }
+           T*New()
+           {
+             if(_lastDelete)
+             {
+               T*object = _lastDelete;
+               _lastDelete =*((T**)(_lastDelete));
+               return new(object) T();
+             }
+             if(_cout >= _end->_objectnum)
+             {
+                  size_t size = 2*_cout;
+                  if(size > _maxcap)
+                  {
+                    size = _maxcap;
+                  }
+                  _end->_next = new BlockNode(size);
+                  _end = _end->next;
+                  _cout = 0;
+             }
+             T*object = (T*)((char*)_end->_memory+_cout*SingleObjNum);
+             _cout++;
+             return new(object) T();
+
+           }
+           void Destory()
+           {
+             BlockNode *cur = _start;
+             while(cur != NULL)
+             {
+               BlockNode *del = cur;
+                 cur = cur->next;
+                 delete del;
+             }
+             _start = _end = NULL;
+           }
+           void Delete(T*object)
+           {
+             if(object)
+             {
+               object->~T();
+               *((T**)object) = _lastDelete;
+               _lastDelete = object;
+             }
+
+             }
+protected:
+           static size_t GetSinglesize()
+           {
+              if(sizeof(T) > sizeof(T*))
+              {
+                return sizeof(T);
+              }
+              else 
+              {
+                return sizeof(T*);
+              }
+           }
 };
+template <class T>
+size_t ObjectPool<T>::SingleObjNum = ObjectPool<T>::GetSinglesize();
 
 int main()
 {
